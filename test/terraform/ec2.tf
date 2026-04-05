d12dfd408c79bf044f4eae7f9879c1332eb30d9e
Kain Kalju <kain@flycom.ee>
2026-04-05 08:34:13 +0300

    using Spot for test instance

--- test/terraform/ec2.tf
+++ test/terraform/ec2.tf
@@ -12,10 +12,22 @@ resource "aws_instance" "test_client" {
     data.aws_security_group.rabbitmq.id,
   ]
 
+  # Hibernate requires an encrypted root volume
   root_block_device {
     volume_type           = "gp3"
     volume_size           = 20
+    encrypted             = true
     delete_on_termination = true
+  }
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior = "hibernate"
+      spot_instance_type             = "persistent"
+      max_price                      = var.spot_max_price
+      valid_until                    = timeadd(timestamp(), "24h")
+    }
   }
 
   tags = {
--- test/terraform/variables.tf
+++ test/terraform/variables.tf
@@ -1,18 +1,34 @@
 variable "region" {
-  default = "eu-north-1"
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
 }
 
+# Ubuntu 24.04 LTS arm64 in eu-north-1
+# Verify with: aws ec2 describe-images --owners 099720109477 \
+#   --filters "Name=name,Values=ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-server-*" \
+#   --query "sort_by(Images,&CreationDate)[-1].ImageId" --region eu-north-1
 variable "ami_id" {
-  description = "Ubuntu 24.04 LTS arm64"
+  description = "Ubuntu 24.04 LTS arm64 AMI ID for eu-north-1"
+  type        = string
   default     = "ami-0ccc95acfa22096b2"
 }
 
 variable "instance_type" {
-  default = "t4g.small"
+  description = "EC2 instance type (ARM64/Graviton3)"
+  type        = string
+  default     = "t4g.small"
+}
+
+variable "spot_max_price" {
+  description = "Maximum hourly Spot price in USD (on-demand t4g.small eu-north-1 is ~$0.0188)"
+  default     = "0.0172"
 }
 
 variable "vpc_id" {
-  default = "vpc-0f47c76ebce94dc7b"
+  description = "Existing default VPC ID"
+  type        = string
+  default     = "vpc-0f47c76ebce94dc7b"
 }
 
 variable "subnet_id" {
@@ -20,18 +36,27 @@ variable "subnet_id" {
   default     = "subnet-01c51fde23794b3af"
 }
 
+# Existing security groups to attach
 variable "sg_ssh_enabled" {
-  default = "sg-064d9ecc1338dad4e"
+  description = "SG ID: ssh-enabled"
+  type        = string
+  default     = "sg-064d9ecc1338dad4e"
 }
 
 variable "sg_from_home" {
-  default = "sg-0207690533a7d530d"
+  description = "SG ID: from-home"
+  type        = string
+  default     = "sg-0207690533a7d530d"
 }
 
 variable "sg_ping" {
-  default = "sg-05d597352554aae74"
+  description = "SG ID: ping"
+  type        = string
+  default     = "sg-05d597352554aae74"
 }
 
 variable "ssh_public_key_path" {
-  default = "~/.ssh/rabbitmq_ed25519.pub"
+  description = "Path to SSH public key to upload as AWS key pair"
+  type        = string
+  default     = "~/.ssh/rabbitmq_ed25519.pub"
 }
